d72bd7f5b784aa3d36c465efce23d399275782ce
tanvirch0750 <tanvir0750@yahoo.com>
2021-02-15 16:10:05 +0600

    implemented sticky navigation

--- script.js
+++ script.js
@@ -132,3 +132,15 @@ const handleHover = function (e) {
 
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+///////////////////////////
+// STICKY NAVIGATION
+const initialCoords = section1.getBoundingClientRect();
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > initialCoords.top) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
+  }
+});
